96a1fa3b58046d9fc41f98d7259e7809a9233f49
alexdecycive <alex@decycive.com>
2017-09-25 22:30:20 -0400

    HTML framework and Firebase init

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,24 @@
+// jquery document iniatlize and page load actions
+$(document).ready(function() {
+console.log("Page loaded, JS started")
+
+//set the DB
+var database = firebase.database();
+
+//create the initial variables and settings
+
+//add a new train scheudule to the DB
+Train Name, Destination, First Trip (HH:MM), Frequency in Minutes
+
+
+//display the train schedule
+---> Train schedule -- 
+Train Name
+Destination
+Frequency (MIN)
+Next Arrival
+Minutes Away
+
+
+//End the document ready function
+});
